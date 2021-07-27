626e61dfb4f12f19bdf6811ec79a130990515bde
Jonathan Nkurunziza <38755447+INGANZAMARUMPU@users.noreply.github.com>
2021-07-27 18:08:26 +0200

    Add files via upload

---
+++ py_to_module.py
@@ -0,0 +1,21 @@
+import os
+
+fichier = input("le fichier à traiter:")
+destination = "".join(fichier.split(".")[:-1])
+os.mkdir(destination)
+
+with open(fichier, "r") as file:
+	new_file = open(os.path.join(destination, "dependancies.py"), "a+")
+	init = open(os.path.join(destination, "__init__.py"), "w+")
+
+	for line in file.readlines():
+		if line.startswith("class ") or line.startswith("def ") :
+			new_file.close()
+			class_name = line.split("(")[0].replace("class ", "").replace("def ", "")
+			init.write(f"from .{class_name} import {class_name}\n")
+			new_file = open(os.path.join(destination, f"{class_name}.py"), "w+")
+			new_file.write("from .dependancies import *\n\n")
+		new_file.write(line)
+	new_file.close()
+	init.close()
+
