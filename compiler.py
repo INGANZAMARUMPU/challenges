ed429e1f6c9f0ff65e4aaa0345bb3c4c62305989
Jonathan Nkurunziza <38755447+INGANZAMARUMPU@users.noreply.github.com>
2021-09-15 10:44:57 +0200

    compiler support all py3 versions

--- compiler.py
+++ compiler.py
@@ -1,5 +1,8 @@
-import compileall
+import compileall, sys
 from pathlib import Path
+
+py_version = "".join(sys.version.split()[0].split(".")[:2])
+CPYTHON_EXT = f".cpython-{py_version}"
 
 print("/!\\ CETTE CHOSE EST VRAIMENT SANS PITIER")
 print("/!\\ IL SUPRIME TOUT LES FICHIER PY")
@@ -21,11 +24,14 @@ for pyc in pycs:
 	parent_dir = pyc.resolve().parent.parent
 
 	old_name = pyc.stem
-	new_name = old_name.replace(".cpython-39", "")
+	new_name = old_name.replace(CPYTHON_EXT, "")
 	
 	try:
 		py = parent_dir / f"{new_name}.py"
 		py.unlink()
+		py = parent_dir / f"{new_name}.pyc"
+		py.unlink()
+
 	except FileNotFoundError as e:
 		continue
 
