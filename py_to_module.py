2874731498b89938ab2c54c55dfa9e2a14e1b66f
Jonathan Nkurunziza <38755447+INGANZAMARUMPU@users.noreply.github.com>
2023-04-07 09:08:39 +0200

    support inline arguments

--- py_to_module.py
+++ py_to_module.py
@@ -1,21 +1,29 @@
 import os
+import sys
 
-fichier = input("le fichier à traiter:")
-destination = "".join(fichier.split(".")[:-1])
-os.mkdir(destination)
+files = []
+if sys.argv[1]:
+	files = sys.argv[1:]
+else:
+	fichier = input("le fichier à traiter:")
+	files.append(fichier)
 
-with open(fichier, "r") as file:
-	new_file = open(os.path.join(destination, "dependancies.py"), "a+")
-	init = open(os.path.join(destination, "__init__.py"), "w+")
+for fichier in files:
+	destination = "".join(fichier.split(".")[:-1])
+	os.mkdir(destination)
 
-	for line in file.readlines():
-		if line.startswith("class ") or line.startswith("def ") :
-			new_file.close()
-			class_name = line.split("(")[0].replace("class ", "").replace("def ", "")
-			init.write(f"from .{class_name} import {class_name}\n")
-			new_file = open(os.path.join(destination, f"{class_name}.py"), "w+")
-			new_file.write("from .dependancies import *\n\n")
-		new_file.write(line)
-	new_file.close()
-	init.close()
+	with open(fichier, "r") as file:
+		new_file = open(os.path.join(destination, "dependancies.py"), "a+")
+		init = open(os.path.join(destination, "__init__.py"), "w+")
+
+		for line in file.readlines():
+			if line.startswith("class ") or line.startswith("def ") :
+				new_file.close()
+				class_name = line.split("(")[0].replace("class ", "").replace("def ", "")
+				init.write(f"from .{class_name} import {class_name}\n")
+				new_file = open(os.path.join(destination, f"{class_name}.py"), "w+")
+				new_file.write("from .dependancies import *\n\n")
+			new_file.write(line)
+		new_file.close()
+		init.close()
 
